9226c2e147957b026f1ccea754c15ea25ece77ac
leondelachaux <80250954+leondelachaux@users.noreply.github.com>
2022-01-11 23:54:47 +0100

    Update train.py

--- train.py
+++ train.py
@@ -71,13 +71,13 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = datasets.CIFAR10(root='~/data', train=True, download=False,
+trainset = datasets.CIFAR10(root='~/data', train=True, download=True,
                             transform=transform_train)
 trainloader = torch.utils.data.DataLoader(trainset,
                                           batch_size=args.batch_size,
                                           shuffle=True, num_workers=8)
 
-testset = datasets.CIFAR10(root='~/data', train=False, download=False,
+testset = datasets.CIFAR10(root='~/data', train=False, download=True,
                            transform=transform_test)
 testloader = torch.utils.data.DataLoader(testset, batch_size=100,
                                          shuffle=False, num_workers=8)
